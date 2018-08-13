7259a45da3a8effa9af415cab8ef83a1b64134ff
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-08-13 14:21:03 +0300

    Update ENForm block to match the refactored blocks code and to be more unit-testable.

--- classes/controller/class-enform-controller.php
+++ classes/controller/class-enform-controller.php
@@ -89,27 +89,39 @@ if ( ! class_exists( 'ENForm_Controller' ) ) {
 		}
 
 		/**
+		 * Get all the data that will be needed to render the block correctly.
+		 *
+		 * @param array  $fields This is the array of fields of the block.
+		 * @param string $content This is the post content.
+		 * @param string $shortcode_tag The shortcode tag of the block.
+		 *
+		 * @return array The data to be passed in the View.
+		 */
+		public function prepare_data( $fields, $content, $shortcode_tag ) : array {
+
+			$data = [
+				'fields' => $fields,
+			];
+			return $data;
+		}
+
+		/**
 		 * Callback for the shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array  $fields This contains array of all data added.
+		 * @param array  $fields This is the array of fields of this block.
 		 * @param string $content This is the post content.
-		 * @param string $shortcode_tag The shortcode block of campaign thumbnail.
+		 * @param string $shortcode_tag The shortcode tag of the block.
 		 *
 		 * @since 0.1.0
 		 *
 		 * @return string All the data used for the html.
 		 */
 		public function prepare_template( $fields, $content, $shortcode_tag ) : string {
-
-			$data = [
-				'fields' => $fields,
-			];
-
-			// Shortcode callbacks must return content, hence, output buffering	here.
+			$data = $this->prepare_data( $fields, $content, $shortcode_tag );
+			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
 			$this->view->block( self::BLOCK_NAME, $data, 'twig', P4EN_INCLUDES_DIR );
-
 			return ob_get_clean();
 		}
 	}
