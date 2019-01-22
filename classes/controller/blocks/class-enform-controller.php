18b339e33aa1f635f3bf119f8d8ba184ed70ac12
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-01-22 18:29:16 +0530

    PLANET-3079 Indentation fix

--- classes/controller/blocks/class-enform-controller.php
+++ classes/controller/blocks/class-enform-controller.php
@@ -192,7 +192,7 @@ if ( ! class_exists( 'ENForm_Controller' ) ) {
 					'label' => __( 'Call to Action button (e.g. "Sign up now!")', 'planet4-engagingnetworks' ),
 					'attr'  => 'button_text_required',
 					'type'  => 'text',
-          'description' => 'Your default is set to [' . __( 'Sign', 'planet4-engagingnetworks' ) . ']',
+					'description' => 'Your default is set to [' . __( 'Sign', 'planet4-engagingnetworks' ) . ']',
 					'meta'  => [
 						'placeholder' => __( 'Enter the "Call to Action" button text', 'planet4-engagingnetworks' ),
 					],
