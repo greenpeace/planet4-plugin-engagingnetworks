a8f5b9ddceb5cc908a6a414798ac8eb6cd84b835
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-06-11 17:15:48 +0300

    v2.2.0

--- planet4-engagingnetworks.php
+++ planet4-engagingnetworks.php
@@ -3,7 +3,7 @@
  * Plugin Name: Planet4 - EngagingNetworks
  * Description: Connects Planet4 with the Engaging Networks platform.
  * Plugin URI: http://github.com/greenpeace/planet4-plugin-engagingnetworks
- * Version: 2.1.0
+ * Version: 2.2.0
  * Php Version: 7.0
  *
  * Author: Greenpeace International
